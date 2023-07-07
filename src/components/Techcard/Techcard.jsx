df868051916245c01fc643c1e7cb084cbb0c7de8
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-07-07 10:29:04 -0600

    Componente tecnologías terminado, trabajando en responsiveness.

--- src/components/Techcard/Techcard.jsx
+++ src/components/Techcard/Techcard.jsx
@@ -5,21 +5,35 @@ import PropTypes from "prop-types";
 function Techcard(props) {
     const [selectedState, setSelectedState] = useState('');
 
+    const colors = props.valcol === "1"
+        ? ["rgba(6, 117, 172, 1)", "rgba(102, 150, 169, 0.8)"]
+        : ["rgba(102, 150, 169, 0.8)", "rgba(6, 117, 172, 1)"];
+
     return (
         <div className={styles.wrapper}>
-            <div className={styles.left}>
+            <div
+                className={styles.left}
+                style={{ backgroundColor: colors[0] }}
+            >
                 <div className={styles.title}>{props.title}</div>
                 <div className={styles.description}>{props.description}</div>
             </div>
-            <div className={styles.right}>
-                <div className={styles.names}>{selectedState}</div>
+            <div
+                className={styles.right}
+                style={{ backgroundColor: colors[1] }}
+            >
+
+                <div className={`${styles.names} ${selectedState !== '' ? styles.selected : ''}`}>
+                    {selectedState}
+                </div>
+
                 <div className={styles.langs}>
                     {props.langs.map((lang, index) => (
                         <div
                             key={index}
                             className={styles.lang}
                             onMouseEnter={() => setSelectedState(lang[1])}
-                            onMouseLeave={() => setSelectedState('')}
+                            onMouseLeave={() => setSelectedState(' ')}
                         >
                             <img className={styles.svg} src={lang[0]} alt="Language Icon" />
                         </div>
--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -8,6 +8,29 @@ const frontend = [
     ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "HTML"],
 ]
 
+const backend = [
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg", "Python"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg", "Java"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg", "C++"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg", "Golang"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg", "PostgreSQL"],
+    ["https://neon-stroopwafel-d8d70e.netlify.app/assets/sqlserver-cfa4fc80.svg", "SQL Server"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg", "Django"],
+    ["https://neon-stroopwafel-d8d70e.netlify.app/assets/ginlogo-6c6f34b3.svg", "Gin-Gonic"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg", "Firebase"],
+]
+
+const others = [
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg", "Kotlin"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg", "Android"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg", "Git"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg", "GitHub"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg", "Linux"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg", "NPM"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original.svg", "Yarn"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jetbrains/jetbrains-original.svg", "JetBrains"],
+]
+
 function Tech() {
     return(
         <div className={styles.wrapper}>
@@ -26,10 +49,22 @@ function Tech() {
             </div>
             <div className={styles.body}>
                 <Techcard
-                    title="Frontend"
+                    title="Front-end"
                     description="Valoro los diseños sencillos, eficientes y atractivos. Me decanto por la estética minimalista y la usabilidad. El diseño es una parte fundamental de mi trabajo y lo que más me gusta hacer."
                     langs={frontend}
                     valcol="1"
+                />
+                <Techcard
+                    title="Back-end"
+                    description="Me enfoco en desarrollar soluciones eficientes y escalables, asegurando un alto rendimiento y una gestión adecuada del lado del servidor."
+                    langs={backend}
+                    valcol="0"
+                />
+                <Techcard
+                    title={"Otras tecnologías"}
+                    description={"Estas son otras tecnologías que he utilizado en mis proyectos:"}
+                    langs={others}
+                    valcol={"1"}
                 />
             </div>
         </div>
