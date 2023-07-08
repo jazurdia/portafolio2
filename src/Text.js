b4e30d1e8745f8ffc2a2aa3d54bd696d621d360f
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-07-08 01:38:20 -0600

    Se pusieron los textos en text.js para en un futuro hacer la versión en inglés.

--- src/Text.js
+++ src/Text.js
@@ -1,13 +1,60 @@
 const aboutmeSP = {
-    name: "Alejandro Azurdia",
+    name: "ALEJANDRO AZURDIA",
     info:
     " Estudiante de Ingeniería en Computación, apasionado por el desarrollo web y apuntando a ser FullStack Developer. Me encanta aprender y siempre estoy buscando nuevos desafíos."
 }
 
-const aboutmeEN = {
-    name: "Alejandro Azurdia",
-    info: "something about me",
+const frontendSP = {
+    title: "Frontend",
+    desc: "Valoro los diseños simples, creativos, eficientes y responsivos. Me decanto por la estética minimalista y la usabilidad.",
 }
 
+const backendSP = {
+    title: "Backend",
+    desc: "Me enfoco en desarollar soluciones eficientes y escalables, asegurando un alto rendimiento y una gestión adecuada del lado del servidor.",
+}
+
+const otherTechSP = {
+    title: "Otras tecnologías",
+    desc: "El desarollo web es un campo muy amplio, por lo que siempre estoy aprendiendo nuevas tecnologías y herramientas para mejorar mis habilidades.",
+}
+
+const frontend = [
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", "JavaScript"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", "React"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", "CSS"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "HTML"],
+]
+
+const backend = [
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg", "Python"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg", "Java"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg", "C++"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg", "Golang"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg", "PostgreSQL"],
+    ["https://neon-stroopwafel-d8d70e.netlify.app/assets/sqlserver-cfa4fc80.svg", "SQL Server"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg", "Django"],
+    ["https://neon-stroopwafel-d8d70e.netlify.app/assets/ginlogo-6c6f34b3.svg", "Gin-Gonic"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg", "Firebase"],
+]
+
+const others = [
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg", "Kotlin"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg", "Android"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg", "Git"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg", "GitHub"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg", "Linux"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg", "NPM"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original.svg", "Yarn"],
+    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jetbrains/jetbrains-original.svg", "JetBrains"],
+]
+
+
+
 export { aboutmeSP }
-export { aboutmeEN }
+export { frontendSP }
+export { backendSP }
+export { otherTechSP }
+export { frontend }
+export { backend }
+export { others }
--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -1,35 +1,8 @@
 import styles from './Tech.module.css'
 import Techcard from '../Techcard/Techcard'
 
-const frontend = [
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", "JavaScript"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", "React"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", "CSS"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", "HTML"],
-]
+import { frontend, frontendSP, backend, backendSP, others, otherTechSP} from "../../Text.js";
 
-const backend = [
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg", "Python"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg", "Java"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg", "C++"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg", "Golang"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg", "PostgreSQL"],
-    ["https://neon-stroopwafel-d8d70e.netlify.app/assets/sqlserver-cfa4fc80.svg", "SQL Server"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg", "Django"],
-    ["https://neon-stroopwafel-d8d70e.netlify.app/assets/ginlogo-6c6f34b3.svg", "Gin-Gonic"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg", "Firebase"],
-]
-
-const others = [
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg", "Kotlin"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg", "Android"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg", "Git"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg", "GitHub"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg", "Linux"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg", "NPM"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original.svg", "Yarn"],
-    ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jetbrains/jetbrains-original.svg", "JetBrains"],
-]
 
 function Tech() {
     return(
@@ -43,20 +16,20 @@ function Tech() {
             </div>
             <div className={styles.body}>
                 <Techcard
-                    title="Front-end"
-                    description="Valoro los diseños sencillos, eficientes y atractivos. Me decanto por la estética minimalista y la usabilidad. El diseño es una parte fundamental de mi trabajo y lo que más me gusta hacer."
+                    title={frontendSP.title}
+                    description={frontendSP.desc}
                     langs={frontend}
                     valcol="1"
                 />
                 <Techcard
-                    title="Back-end"
-                    description="Me enfoco en desarrollar soluciones eficientes y escalables, asegurando un alto rendimiento y una gestión adecuada del lado del servidor."
+                    title={backendSP.title}
+                    description={backendSP.desc}
                     langs={backend}
                     valcol="0"
                 />
                 <Techcard
-                    title={"Otras tecnologías"}
-                    description={"Estas son otras tecnologías que he utilizado en mis proyectos:"}
+                    title={otherTechSP.title}
+                    description={otherTechSP.desc}
                     langs={others}
                     valcol={"1"}
                 />
